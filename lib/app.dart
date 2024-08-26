5bfe5b4a2fb4a364f236d2e243ef045add22f327
hegleB <dltkd1385@gmail.com>
2024-08-26 16:00:10 +0900

    feat(#2): firebase dio 추가

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:fishingmemory/core/data/api/%08dio_util.dart';
+import 'package:fishingmemory/core/data/api/auth_service.dart';
+import 'package:fishingmemory/core/data/repository/auth/auth_repository.dart';
+import 'package:fishingmemory/feature/login/bloc/login_bloc.dart';
 import 'package:fishingmemory/feature/login/view/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
+    final dio = DioUtil();
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(
+          create: (context) => AuthRepositoryImpl(
+            authService: AuthService(dio.firebaseDio),
+          )
+        ),
+      ], 
+      child: BlocProvider(
+        create: (context) => LoginBloc(
+            authRepository: RepositoryProvider.of<AuthRepository>(context),
+          ),
+        child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),  
+      )
+    )
+  );
   }
 }
