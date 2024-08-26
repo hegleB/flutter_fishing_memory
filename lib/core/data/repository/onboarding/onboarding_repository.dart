8b36541474462a5643f107be70a1f09b8cfb8e30
hegleB <dltkd1385@gmail.com>
2024-08-26 23:34:48 +0900

    chore(#3): shared preferences 라이브러리 추가

---
+++ lib/core/data/repository/onboarding/onboarding_repository.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class OnboardingRepository {
+  Future<String?> readOnboarding(String key);
+  Future<void> writeOnboarding(String key, String value);
+}
+
+class OnboardingRepositoryImpl implements OnboardingRepository {
+  final SharedPreferences sharedPreferences;
+
+  OnboardingRepositoryImpl(this.sharedPreferences);
+
+  @override
+  Future<String?> readOnboarding(String key) async {
+    try {
+      return sharedPreferences.getString(key);
+    } catch (e) {
+      print("Error reading from SharedPreferences: $e");
+      return null;
+    }
+  }
+
+  @override
+  Future<void> writeOnboarding(String key, String value) async {
+    try {
+      await sharedPreferences.setString(key, value);
+    } catch (e) {
+      print("Error writing to SharedPreferences: $e");
+    }
+  }
+}
