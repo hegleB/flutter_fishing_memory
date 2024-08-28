99cc94be6ec61eeba0d86f4bb0f087a39f979b73
hegleB <dltkd13956@naver.com>
2024-08-29 01:27:28 +0900

    feat: fishing spot screen 클래스 추가

---
+++ lib/feature/fishingspot/view/fishing_spot_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FishingSpotScreen extends StatelessWidget {
+  const FishingSpotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("낚시터 찜")),
+    );
+  }
+}
