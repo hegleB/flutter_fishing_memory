ae7edf9fdf0f3e652702d93b68c66dc85bbad312
hegleB <dltkd13956@naver.com>
2024-08-29 16:18:47 +0900

    feat: 다크모드 화면 기본 UI 틀 구현

---
+++ lib/feature/mypage/view/dark_mode_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class DarkModeScreen extends StatelessWidget {
+  const DarkModeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("다크모드")),
+    );
+  }
+}
